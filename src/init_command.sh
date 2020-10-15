c321eabc39784c79f11d6f6153f5d3eb0fcb7a37
Lucas Carlson <lucas@carlson.net>
2020-10-14 17:20:53 -0700

    Adding init

--- src/init_command.sh
+++ src/init_command.sh
@@ -1,1 +1,1 @@
-base64 < /dev/urandom | tr -d 'O0Il1+/' | head -c 64 > .cloudenv_secret_key ; echo >> .cloudenv_secret_key
+base64 < /dev/urandom | tr -d 'O0Il1+/' | head -c 64 > .cloudenv-secret-key ; echo >> .cloudenv-secret-key
