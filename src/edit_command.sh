a2198685852a95f9950d401a606bc5d561092f78
Lucas Carlson <lucas@carlson.net>
2020-10-14 17:03:54 -0700

    Adding basic commands

---
+++ src/edit_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/edit_command.sh'"
+echo "# code for 'cloudenv edit' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
