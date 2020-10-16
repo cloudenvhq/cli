c36d775c74a75cfc6a376f6683df4e3736fbdc72
Lucas Carlson <lucas@carlson.net>
2020-10-16 14:55:33 -0700

    Fixing init

--- src/init_command.sh
+++ src/init_command.sh
@@ -12,7 +12,7 @@ bearer=`cat ~/.cloudenvrc`
 
 if [ -f .cloudenv-secret-key ]
 then
-	echo "Already found existing CloudEnv project in $PWD/.cloudenv-secret-key"
+	echo "Already found an existing CloudEnv project in $PWD/.cloudenv-secret-key"
 	echo
 	read -p "Generate a new secret key for this project? (N/y): " newkey
 	if [ "$newkey" == "y" ]
@@ -21,17 +21,54 @@ then
 		base64 < /dev/urandom | tr -d 'O0Il1+/' | head -c 256 >> .cloudenv-secret-key-new
 		echo >> .cloudenv-secret-key-new
 		mv .cloudenv-secret-key-new .cloudenv-secret-key
-		keysha=`openssl dgst -sha256 .cloudenv-secret-key`
-		curl -s -H "Authorization: Bearer $bearer" "https://app.cloudenv.com/api/v1/apps/update?name=$name&keysha=$keysha" > /tmp/cloudenv-app
-		echo
-		echo "SUCCESS: New encryption key generated, you will now need to distribute $PWD/.cloudenv-secret-key to all your team members and servers"
+		name=`head -1 .cloudenv-secret-key`
+		sha=`openssl dgst -sha256 .cloudenv-secret-key`
+	    read -ra ADDR <<< "$sha"
+		app=`curl -s -H "Authorization: Bearer $bearer" "https://app.cloudenv.com/api/v1/apps?name=$name&sha=${ADDR[1]}"`
+		if [ "$app" != 200 ]
+		then
+			echo
+			echo "ERROR ($app): I'm sorry but you do not have access to this app. If you believe this is in error, please contact another team member."
+		else
+			echo
+			echo "SUCCESS: New encryption key generated"
+			echo
+			echo "You need to distribute $PWD/.cloudenv-secret-key to all your team members and servers"
+		fi
 	fi
 else
 	read -p "Name of App: " name
-	curl -s -H "Authorization: Bearer $bearer" "https://app.cloudenv.com/api/v1/apps/get?name=$name" > /tmp/cloudenv-app
+	app=`curl -s -H "Authorization: Bearer $bearer" "https://app.cloudenv.com/api/v1/apps?name=$name"`
+	if [ "$app" == 401 ]
+	then
+		echo
+		echo "ERROR ($app): This app name already exists, please choose a different one and try again."
+		exit
+	fi
+	if [ "$app" == 200 ]
+	then
+		echo
+		echo "This app name already exists."
+		echo
+		echo "To get access to the variables, you must get a copy of .cloudenv-secret-key from a team member into this directory"
+		echo
+		exit
+	fi
 	echo $name > .cloudenv-secret-key
 	base64 < /dev/urandom | tr -d 'O0Il1+/' | head -c 256 >> .cloudenv-secret-key
 	echo >> .cloudenv-secret-key
-	keysha=`openssl dgst -sha256 .cloudenv-secret-key`
-	curl -s -H "Authorization: Bearer $bearer" "https://app.cloudenv.com/api/v1/apps/update?name=$name&keysha=$keysha" > /tmp/cloudenv-app
+	sha=`openssl dgst -sha256 .cloudenv-secret-key`
+    read -ra ADDR <<< "$sha"
+	curl -s -H "Authorization: Bearer $bearer" "https://app.cloudenv.com/api/v1/apps?name=$name&sha=${ADDR[1]}" > /tmp/cloudenv-app
+	if [ "$app" != 200 ]
+	then
+		echo
+		echo "ERROR ($app): This app name already exists, please choose a different one and try again."
+		exit
+	else
+		echo
+		echo "SUCCESS: You have created the app '$name' in CloudEnv"
+		echo
+		echo "You need to distribute $PWD/.cloudenv-secret-key to all your team members and servers"
+	fi
 fi
