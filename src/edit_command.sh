5ac41e8348c81fde76b3209ba286928001ced0ae
Lucas Carlson <lucas@carlson.net>
2020-10-16 07:20:35 -0700

    Adding edit command

--- src/edit_command.sh
+++ src/edit_command.sh
@@ -1,7 +1,25 @@
-if [[ "${args[--edit]}" ]]; then
-  editor="${EDITOR:-vi}"
-  "$editor" "$CONFIG_FILE"
-else
-  cyan "# $CONFIG_FILE"
-  config_show
+if [ ! -f .cloudenv-secret-key ]
+then
+	echo
+	echo "Couldn't find a cloudenv project in $PWD/.cloudenv-secret-key"
+	echo
+	echo "Please run: cloudenv init"
+	echo
+	echo "Or cd into the root directory of your app to make env edits"
+	echo
+	exit
 fi
+
+editor="${EDITOR:-nano}"
+bearer=`cat ~/.cloudenvrc`
+project=`head -1 .cloudenv-secret-key`
+secretkey=`tail -1 .cloudenv-secret-key`
+environment="${args[environment]}"
+
+curl -s -H "Authorization: Bearer $bearer" "https://app.cloudenv.com/api/v1/get_env.json?project=$project&environment=$environment" > /tmp/cloudenv-edit
+
+"$editor" /tmp/cloudenv-edit
+
+curl -s -H "Authorization: Bearer $bearer" -F "data=@/tmp/cloudenv-edit" "https://app.cloudenv.com/api/v1/put_env.json?project=$project&environment=$environment"
+
+rm /tmp/cloudenv-edit
--- src/init_command.sh
+++ src/init_command.sh
@@ -1,2 +1,20 @@
-base64 < /dev/urandom | tr -d 'O0Il1+/' | head -c 256 > .cloudenv-secret-key
-echo >> .cloudenv-secret-key
+if [ -f .cloudenv-secret-key ]
+then
+	echo "Already found existing CloudEnv project in $PWD/.cloudenv-secret-key"
+	echo
+	read -p "Generate a new secret key for this project? (N/y): " newkey
+	if [ "$newkey" == "y" ]
+	then
+		head -1 .cloudenv-secret-key > .cloudenv-secret-key-new
+		base64 < /dev/urandom | tr -d 'O0Il1+/' | head -c 256 >> .cloudenv-secret-key-new
+		echo >> .cloudenv-secret-key-new
+		mv .cloudenv-secret-key-new .cloudenv-secret-key
+		echo
+		echo "SUCCESS: New encryption key generated, you will now need to distribute $PWD/.cloudenv-secret-key to all your team members and servers"
+	fi
+else
+	read -p "Name of App: " name
+	echo $name > .cloudenv-secret-key
+	base64 < /dev/urandom | tr -d 'O0Il1+/' | head -c 256 >> .cloudenv-secret-key
+	echo >> .cloudenv-secret-key
+fi
