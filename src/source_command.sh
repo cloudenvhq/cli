7c3893bd8c36902d521334cf84a6fb23242cbdf2
Lucas Carlson <lucas@carlson.net>
2021-02-15 08:22:08 -0800

    Fixing source to be more flexible

--- src/source_command.sh
+++ src/source_command.sh
@@ -4,6 +4,7 @@ check_logged_in
 check_for_project
 check_can_read_env
 
+env_file=`mktemp`
 IFS=$'\n'
 re=^\s*[A-Za-z_][A-Za-z0-9_]*=
 
@@ -11,19 +12,21 @@ if [[ "$environment" != "default" ]]
 then
   for line in $(get_env "default"); do
     if [[ $line =~ $re ]]; then
-      echo "export $line"
+      echo "export $line" >> $env_file
     else
-      echo "$line"
+      echo "$line" >> $env_file
     fi
   done
 fi
 
 for line in $(get_env "$environment"); do
   if [[ $line =~ $re ]]; then
-    echo "export $line"
+    echo "export $line" >> $env_file
   else
-    echo "$line"
+    echo "$line" >> $env_file
   fi
 done
+
+echo ". $env_file && rm $env_file"
 
 rm -rf "$tempdir"
